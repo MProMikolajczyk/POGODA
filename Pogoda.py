55a1a88aebacc7176654da4670408c33629722a3
mmpro <marekmikolajczyk87@gmail.com>
2018-10-27 15:57:29 +0200

    Uladzono projekt.Wprowadzono classe przeszukiwanie danych+ pogrupowano funkcje

--- Pogoda.py
+++ Pogoda.py
@@ -5,16 +5,37 @@
 #-*- coding: utf-8 -*-
 
 #------------------------Pozyskiwanie danych------------------------------------
-from lxml import html
-import requests
-import re
+class Pozyskiwane_dane(object):
 
-#Wariant I- Akutualnie pobierane dane
-przeszukiwana_strona_1 = requests.get('http://pruszkow.infometeo.pl')
-tree_strukura_html = html.fromstring(przeszukiwana_strona_1.content)
-wyszukany_zboir_danych =''.join(tree_strukura_html.xpath('/html/body/div/pre/text()'))
+    from lxml import html
+    import requests
+    import re
 
-                    #wyszukiwane zmienne
-temp=int(re.search(r"TEMPERATURA.+\b([0-9][0-9]?)",wyszukany_zboir_danych).group(1))
-opady=re.search(r"CHMURY[.]+.([\w.]*.[\w?.]*)",wyszukany_zboir_danych).group(1)
+    def __init__(self):
+        self.strony={'http://pruszkow.infometeo.pl':'/html/body/div/pre/text()'}
 
+#--------Wariant_1-------
+    def przeszukiwana_strona(self):
+        for strona in self.strony:
+            wybrana_strona=self.requests.get(strona)
+            html_content=self.html.fromstring(wybrana_strona.content)
+            stuktura_html=''.join(html_content.xpath(self.strony[strona]))
+            return stuktura_html
+
+    def akutualna_temp(self):
+        temp=int(self.re.search(r"TEMPERATURA.+\b([0-9][0-9]?)",self.przeszukiwana_strona()).group(1))
+        return temp
+
+    def aktualne_opady(self):
+        opady=self.re.search(r"CHMURY[.]+.([\w.]*.[\w?.]*)",self.przeszukiwana_strona()).group(1)
+        return opady
+
+#--------Wariant_2-------
+'''
+Gdy nie pobiera danych to ma sobie je sam uzupełnić 
+'''
+
+#----------------------------------Główna zawartość---------------------------
+def main():
+    pozyskiwane_dane = Pozyskiwane_dane()
+main()
