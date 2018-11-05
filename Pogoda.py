d027fb170a811a69f55999db6474e691822f8abc
mmpro <marekmikolajczyk87@gmail.com>
2018-11-05 17:33:05 +0100

    sortowanie+ plik pogoda_sort

--- Pogoda.py
+++ Pogoda.py
@@ -53,7 +53,7 @@ class Operacje_na_plikach(object):
 
     def __init__(self):
         self.nazwy_kolumn=["TEMPERATURA","OPAD","DATA","CZAS","ID"]
-        self.link = list(self.odczyt_danych_z_wierszy())
+        self.pliki = ["pogoda_data.csv","pogoda_data_sort.csv"]
 
 
     #dopisywanie
@@ -63,41 +63,52 @@ class Operacje_na_plikach(object):
             write.writerow({"TEMPERATURA": dane_1, "OPAD":dane_2,"DATA":self.datetime.datetime.now().strftime('%Y-%m-%d'),"CZAS":self.datetime.datetime.now().strftime('%H:%M:%S')})
 
     def dopisanie_do_pliku_sort(self):
-        with open("pogoda_data_sort.csv", "a", newline="", encoding='utf-8') as csvfile:
+        with open("pogoda_data_sort.csv", "w", newline="", encoding='utf-8') as csvfile:
             write = self.csv.DictWriter(csvfile, fieldnames=self.nazwy_kolumn)
-            godz = self.datetime.timedelta(hours=0)
-            while godz < self.datetime.timedelta(hours=24):
+            write.writeheader()
+            godz = self.datetime.datetime(2018, 11, 5, 0, 0)
+            while godz <= self.datetime.datetime(2018, 11, 5, 23, 59):
                 for ID in range(0, 96):
-                    write.writerow({"DATA": self.datetime.datetime.now().strftime('%Y-%m-%d'), "CZAS": godz, "ID": ID})
+                    write.writerow({"DATA": self.datetime.datetime.now().strftime('%Y-%m-%d'), "CZAS": godz.strftime('%H:%M:%S'), "ID": ID})
                     godz += self.datetime.timedelta(minutes=15)
+        self.nadpisywanie()
 
-    #wyswietlanie danych
-    def wyswietlanie_danych(self):
-        for wyswietl in self.link:
-            print(wyswietl)
+    def nadpisywanie(self):
+        value='5'
+        with open('pogoda_data_sort.csv', 'r') as readfile:
+            reader = self.csv.reader(readfile)
+            lines = list(reader)
+            time='23:44:33'
+            for pozycja in range(2,97):
+                if time >= lines[pozycja-1][3] and time < lines[pozycja][3]:
+                    lines[pozycja-1][0]=value
+            if time >= lines[96][3]:
+                lines[96][0] = value
+        with open('pogoda_data_sort.csv', 'w') as writefile:
+            writer = self.csv.writer(writefile)
+            writer.writerows(lines)
 
     #operacje na kolumnach
-    def odczyt_danych_z_kolumn(self,nr_kolumny=True):
-        with open("pogoda_data.csv", "r") as csvfile:
+    def odczyt_danych_z_kolumn(self,nr_kolumny=True,nr_plik=0):
+        with open(self.pliki[nr_plik], "r") as csvfile:
             reader =self.csv.DictReader(csvfile)
             for row in reader:
                 yield(row[self.nazwy_kolumn[nr_kolumny]])
 
-
-    def zakres_kolumn(self,start=0, finish=True,nr_kolumny=True): #wyniki od start do finish z dla nr kolumny
-        zakres = list(self.odczyt_danych_z_kolumn(nr_kolumny))
+    def zakres_kolumn(self,start=0, finish=True,nr_kolumny=True,nr_plik=0): #wyniki od start do finish z dla nr kolumny
+        zakres = list(self.odczyt_danych_z_kolumn(nr_kolumny,nr_plik))
         for wiersz in range(start, finish):
             yield zakres[wiersz]
 
     #operacje na wierszach
-    def odczyt_danych_z_wierszy(self):
-        with open("pogoda_data.csv", "r") as csvfile:
+    def odczyt_danych_z_wierszy(self,nr_plik=0):
+        with open(self.pliki[nr_plik], "r") as csvfile:
             reader =self.csv.DictReader(csvfile)
             for row in reader:
                     yield row
 
-    def zakres_wierszy(self,start=0, finish=True): #wyniki od start do finish dla wszystkich wierszy
-        zakres = list(self.odczyt_danych_z_wierszy())
+    def zakres_wierszy(self,start=0, finish=True,nr_plik=0): #wyniki od start do finish dla wszystkich wierszy
+        zakres = list(self.odczyt_danych_z_wierszy(nr_plik))
         for wiersz in range(start, finish):
             yield zakres[wiersz]
 
